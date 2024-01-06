aebb2efad420443a7e9b67cf991724ddd222f649
Nicolas Bolañez <Zven94>
2024-01-06 19:05:52 -0300

    changed searchForm listener and added timer to avoid multiple calls to the API

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
 document.addEventListener('DOMContentLoaded', function () {
 
   const searchForm = document.querySelector('.searchForm');
+  const previousButton = document.querySelector('.previousR');
+  const nextButton = document.querySelector('.nextR');
 
-  searchForm.addEventListener('submit', function (e) {
+  // index to controll showing results
+  let start_index = 1;
+
+  // create variable for the setTimeOut counter to avoid multiple calls to the API
+  let debounceTimer;
+
+  searchForm.addEventListener('input', function (e) {
     e.preventDefault();
 
-    const searchInput = document.querySelector('.searchInput');
+    // clear the timer when function is called
+    clearTimeout(debounceTimer);
+
+    // set a timer to call the API after 1 second
+    debounceTimer = setTimeout(function () {
+      updateSearch(start_index)
+    }, 1000);
+  });
+
+  // manage previuos resutls
+  previousButton.addEventListener('click', function (e) {
+    if (start_index > 1) {
+      updateSearch(start_index - 10)
+    }
+  });
+
+  nextButton.addEventListener('click', function (e) {
+    updateSearch(start_index + 10)
+  });
+
+
+  // update index and fetch data
+
+  function updateSearch(newIndex) {
+    start_index = newIndex;
+
+    const searchInput = document.querySelector('.searchInput').value.trim();
 
     // build my url to use the search API
-    let url = 'https://article-searcher.onrender.com/search?q=' + encodeURIComponent(searchInput.value);
-
+    let url = 'https://article-searcher.onrender.com/search?q=' + encodeURIComponent(searchInput) + `&start_index=${start_index}`;
+    console.log(url);
     // make the request to the API
     fetch(url)
       .then(function (response) {
@@ -24,19 +58,19 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log(error);
       });
 
-      searchInput.value = '';
-  });
+    // searchInput.value = '';
+  }
 
   function displayResults(results) {
     let showResults = document.querySelector('.showResults');
-    
+
     // clean up the results from previus search
     showResults.innerHTML = '';
 
     // loop over the results
     results.forEach(result => {
       const article = document.createElement('li');
-      
+
       const title = document.createElement('h3');
       title.textContent = result.title;
       const link = document.createElement('a');
